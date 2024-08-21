dd480f7d9ac2a54e2ad0f29debdfd5ab0a3b2b50
Sang Fan <phamthaisang1710@gmail.com>
2024-08-21 20:01:41 +0700

    Update Assignment1.java

--- src/assignment1/Assignment1.java
+++ src/assignment1/Assignment1.java
@@ -2,12 +2,11 @@ import java.util.Scanner; // Import the Scanner class
 
 public class Assignment1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); // Create a Scanner object
-        int n = sc.nextInt(); // Read user input
-        sc.close(); // Close the Scanner to avoid resource leaks
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap so nguyen);
+        int n = sc.nextInt();
         
-        for (int i = n; i >= -n; i--) {
-            System.out.print(i + " "); // Print each number followed by a space
-        }
+        System.out.print(n);
+       
     }
 }

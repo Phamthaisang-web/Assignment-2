50ec8d57800c92398247d4ded024f5caf95092f4
Sang Fan <phamthaisang1710@gmail.com>
2024-08-21 22:00:26 +0700

    Update Assignment1.java

--- src/assignment1/Assignment1.java
+++ src/assignment1/Assignment1.java
@@ -3,10 +3,14 @@ import java.util.Scanner; // Import the Scanner class
 public class Assignment1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Nhap so nguyen);
-        int n = sc.nextInt();
-        
-        System.out.print(n);
-       
+        System.out.println("Nhap so nguyen a");
+        int a = sc.nextInt();
+        System.out.println("Nhap so nguyen b");
+        int b = sc.nextInt();
+        System.out.println("a + b = "+ (a+b));
+        System.out.println("a - b = "+ (a-b));
+        System.out.println("a * b = "+ (a*b));
+        System.out.println("a / b = "+ (a/b));
+        System.out.println("a % b = "+ (a%b));
     }
 }

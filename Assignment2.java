a1484e5058871342ba171347048fb9b2f98cafe1
Sang Fan <phamthaisang1710@gmail.com>
2024-08-22 15:03:50 +0700

    Update Assignment2.java

--- Assignment2.java
+++ Assignment2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Assignment1 {
+public class Assignment2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Nhap so nguyen a");
